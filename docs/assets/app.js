7878544df58923271e7286bb167118dbedf0bca1
f1shy-dev <agarwal.vrishank@gmail.com>
2022-01-15 20:51:12 +0000

    comments and remove log

--- docs/assets/app.js
+++ docs/assets/app.js
@@ -6,14 +6,17 @@ const hide = (h) => show(h, "none");
 const innerText = (i, t) => (qs(i).innerText = t);
 
 const app = async () => {
+  // convert bookURL from query into url for kerboodle book data
   const bookURL = atob(new URLSearchParams(window.location.search).get("c"));
   const split = bookURL.split("/");
   const dataURL = split.slice(0, split.length - 1).join("/") + "/data.js";
 
+  // get the book data and parse it as JSON
   const rawData = await (await fetch(dataURL)).text();
   const trimmed = rawData.substring(11, rawData.length - 1);
   const data = JSON.parse(trimmed);
 
+  // read the XML file inside the JSON, and sanitise the page
   const parser = new DOMParser();
   const doc = parser.parseFromString(
     data["LearningObjectInfo.xml"],
@@ -21,7 +24,6 @@ const app = async () => {
   );
   const rawPages = Array.from(doc.querySelector("pages").children);
   const pages = rawPages.map((elem) => elem.children[0].innerHTML);
-  console.log(doc);
 
   qs(".bookImg").src = pages[0];
   innerText(".bookTitle", doc.querySelector("title").innerHTML);
